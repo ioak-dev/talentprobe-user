c6a222ef1e38fad4b74c9df56767d6f498892cc0
Arunkumar Selvaraj <arun@ioak.io>
2024-07-15 12:29:16 +0530

    UI design for assessment page

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -9,8 +9,19 @@ interface Props {}
 
 const MainPage = (props: Props) => {
   return (
-    <div className="main-page page-container">
-      <UserForm />
+    <div className="main-page-container">
+      <div className="main-page">
+        <div className="main-page__left">
+        <p><b>Question 1 of 10</b></p>
+          <h2>Fill in your details to start the assessment</h2>
+          <p>Select one answer</p>
+        </div>
+        <div className="main-page__right">
+          <div>
+          <UserForm />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
